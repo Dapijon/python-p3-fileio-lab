9a863c45a7455d57aceb760497d7a79aeff3f653
leonkinyanjui <dapijon@gmail.com>
2023-12-05 13:34:53 +0300

    Assignment complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,28 @@
 def write_file(file_name, file_content):
-    pass
+    try:
+        with open(f'{file_name}.txt', 'w') as file:
+            file.write(file_content)
+        print(f"File '{file_name}.txt' successfully written.")
+    except Exception as e:
+        print(f"Error writing to file '{file_name}.txt': {e}")
+        pass
 
-def append_file(file_name, append_content):
-    pass
+def append_to_file(file_name, file_content):
+    try:
+        with open(f'{file_name}.txt', 'a') as file:
+            file.write(file_content)
+        print(f"Content appended to file '{file_name}.txt'.")
+    except Exception as e:
+        print(f"Error appending to file '{file_name}.txt': {e}")
+        pass
 
 def read_file(file_name):
+    try:
+        with open(f'{file_name}.txt', 'r') as file:
+            content = file.read()
+        return content
+    except Exception as e:
+        print(f"Error reading file '{file_name}.txt': {e}")
+        return None
+
     pass
